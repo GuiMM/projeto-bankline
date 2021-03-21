1bd2a63109c8f8a2d64dd1b22ce4d53b5ec6b4b1
felipe Seabra <felipeks25@gmail.com>
2021-03-21 02:46:37 -0300

    feature: Criacao de controller Dashboard, ajustes na validacao de criacao de usuario e feito metodo update senha

---
+++ src/main/java/com/bankline/controller/DashboardController.java
@@ -0,0 +1,39 @@
+package com.bankline.controller;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bankline.dto.DashboardRequestDto;
+import com.bankline.dto.DashboardResultDto;
+import com.bankline.service.DashboardService;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+	@Autowired
+	DashboardService dashboardService;
+	@Autowired
+	DashboardRequestDto dashboardRequestDto;
+
+	@Bean
+	public DashboardRequestDto dashboardRequestDto() {
+		return new DashboardRequestDto();
+	}
+
+	@GetMapping("/{login}/{dataInicio}/{dataFim}")
+	public DashboardResultDto dashboardList(@PathVariable String login, @PathVariable String dataInicio,
+			@PathVariable String dataFim) throws ParseException {
+		dashboardRequestDto.setLogin(login);
+		dashboardRequestDto.setDataInicio(dataInicio);
+		dashboardRequestDto.setDataFim(dataFim);
+		return dashboardService.getDashboard(dashboardRequestDto);
+	}
+}
